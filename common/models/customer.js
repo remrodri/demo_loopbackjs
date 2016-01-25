ee52e0358fc3501fa433a406c623f5f8617aa42a
Roli B <rolib2@gmail.com>
2016-01-25 12:54:21 -0400

    demo code

---
+++ common/models/customer.js
@@ -0,0 +1,14 @@
+module.exports = function(Customer) {
+
+
+  Customer.hello = function(ms, cb){
+  	cb(null, 'hello '+ms);
+  };
+
+  Customer.remoteMethod('hello',
+  {
+  	accepts: { arg:'ms', type:'string' },
+  	returns: { arg: 'saludo', type:'string' }
+  })
+
+};
